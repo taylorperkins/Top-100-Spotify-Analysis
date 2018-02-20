88fcdab951307ed9a8dc56be9e10b773a041ccc1
taylorperkins <taylorperkins.dev@gmail.com>
2018-02-19 19:41:14 -0600

    Finished up the cleaning process for song-ranked-analytics.. for now..

--- r-scripts/clean/song-ranked-analytics.R
+++ r-scripts/clean/song-ranked-analytics.R
@@ -1,4 +1,6 @@
-library("dplyr")
+library(dplyr)
+library(foreach)
+library(iterators)
 
 #############################
 # 
@@ -15,7 +17,7 @@ library("dplyr")
 # song_id          unique song id for billboards api (not used by me)
 # song_name        name of song
 # artist_id        unique artist id for billboards api (not used by me)
-# spotify_api      unique song id for spotify api
+# spotify_id      unique song id for spotify api
 # rank             rank given
 # 
 # SPOTIFY API
@@ -44,5 +46,81 @@ library("dplyr")
 #############################
 # READ IN SONG RANKED ANALYTICS
 #############################
-sra <- read.csv('./data/ranks/song-ranked-analytics.csv')
+# cols_to_exclude <- c('analysis_url', 'track_href', 'type', 'uri', 'X')
+# sra <- read.csv('./data/ranks/song-ranked-analytics.csv')
+# 
+# sra <- sra[, !(names(sra) %in% cols_to_exclude)]
 
+
+#############################
+# START CLEANING PROCESS
+#############################
+
+# DONT RUN THESE VVV, READ IN THE FILE INSTEAD
+
+# Find all of the rows where there is no spotify id. There are 416 unique artists where no spotify id is available. 
+# Since there is no id tied to the record, I am not able to dive into the analysis as much as I would like. 
+# I am going to save these off to dig into further at another time. 
+# missing_spotify_id <- sra[(is.na(sra$spotify_id) | sra$spotify_id == ''),]
+# missing_spotify_id %>% 
+#   distinct(display_artist) %>% 
+#   arrange(desc(display_artist))
+# 
+# missing_spotify_id %>% 
+#   select(date, song_id, song_name, artist_id, spotify_id, rank) %>% 
+#   write.csv('./data/ranks/song-ranked-analytics-missing-spotify-id.csv')
+# 
+# # CONVERTING FACTORS TO CHARACTERS
+# i <- sapply(sra, is.factor)
+# sra[i] <- lapply(sra[i], as.character)
+# 
+# sra <- sra[!(is.na(sra$spotify_id) | sra$spotify_id == ''), ] %>% 
+#   arrange(date, rank)
+# 
+# sra$date <- as.POSIXlt(sra$date)
+# 
+# update_song_previously_ranked <- list()
+# update_artist_previously_ranked <- list()
+# 
+# for (name in unique(sra$spotify_id)) update_song_previously_ranked[[ name ]] <- 0
+# for (name in unique(sra$display_artist)) update_artist_previously_ranked[[ name ]] <- 0
+# 
+# sra$song_previously_ranked <- 0
+# sra$artist_previously_ranked <- 0
+# 
+# for (ind in seq(from = 1, to = nrow(sra))) {
+#   
+#   print(ind)
+#   
+#   current_row <- sra[ind, c("spotify_id", "display_artist")]
+#   
+#   sra[ind, 'song_previously_ranked'] <- update_song_previously_ranked[[ current_row[[ "spotify_id" ]] ]]
+#   sra[ind, 'artist_previously_ranked'] <- update_artist_previously_ranked[[ current_row[[ "display_artist" ]] ]]
+#   
+#   update_song_previously_ranked[[ current_row[[ "spotify_id" ]] ]] <- update_song_previously_ranked[[ current_row[[ "spotify_id" ]] ]] + 1
+#   update_artist_previously_ranked[[ current_row[[ "display_artist" ]] ]] <- update_artist_previously_ranked[[ current_row[[ "display_artist" ]] ]] + 1
+#   
+# }
+
+# saveRDS(sra, './data/r-objects/song-ranked-analytics/sra-cleanup-1.rds')
+sra <- readRDS('./data/r-objects/song-ranked-analytics/sra-cleanup-1.rds')
+
+# ADD IN YEAR AND MONTH
+sra <- sra %>% 
+  mutate(
+    year = strftime(date, '%Y'),
+    month = strftime(date, '%b')
+  )
+
+# ADD KEY NAMES PER KEY
+key_num <- c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+key_names <- c('C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B')
+
+key_map <- setNames(as.list(key_names), key_num)
+
+sra <- sra %>% 
+  mutate(
+    key_name = key_names[match(key, key_num)]
+  )
+
+saveRDS(sra, './data/r-objects/song-ranked-analytics/song-ranked-analytics.rds')
